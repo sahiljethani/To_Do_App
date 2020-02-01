8ecc7cc72055b33ba8720394e657e7ec309f684f
sahiljethani <sahil.jethani@gmail.com>
2020-02-01 13:57:39 +0530

    Recycler View Start

---
+++ app/src/main/java/com/example/todoapp/ListAdapter.java
@@ -0,0 +1,27 @@
+package com.example.todoapp;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder>{
+
+
+    
+
+
+
+
+
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
